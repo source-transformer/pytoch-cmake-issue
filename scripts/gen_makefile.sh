54b9364ae2f33a3efc5973f25d83ea922c8e0613
Tim Cassidy <cassidy@dropfake.com>
2023-02-12 08:34:41 -0800

    updated path

--- scripts/gen_makefile.sh
+++ scripts/gen_makefile.sh
@@ -1,6 +1,6 @@
 mkdir -p ../build
 pushd ../build
 
-cmake -DCMAKE_PREFIX_PATH=/Users/tcassidy/gitlab/omg-pytorch-mac-arm64/torch/ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -DCMAKE_PREFIX_PATH=/packages/omg-pytorch-mac-arm64/torch/ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 
 popd
